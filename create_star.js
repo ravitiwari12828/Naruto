d6a749b0eb541fc9a0b797727798c2e41f1c2eca
Ravi Kumar Tiwari <ravitiwari12828@gmail.com>
2026-07-24 20:04:21 +0530

    fix-oom-increase-heap-128mb-to-512mb-for-39-modules

--- create_star.js
+++ create_star.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 try {
-  fs.writeFileSync('*.js', "const v8 = require('v8'); try { v8.setFlagsFromString('--max_old_space_size=128'); } catch(e){} require('./src/index.js');\n");
+  fs.writeFileSync('*.js', "const v8 = require('v8'); try { v8.setFlagsFromString('--max_old_space_size=512'); } catch(e){} require('./src/index.js');\n");
   console.log('Created *.js entrypoint file!');
 } catch (err) {}
