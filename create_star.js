4145b9f1cee46e09b3d39e0fdacfc5adf3dd6dc7
Ravi Kumar Tiwari <ravitiwari12828@gmail.com>
2026-07-24 23:58:13 +0530

    fix-bulletproof-endercloud-startup-clean-package-json

--- create_star.js
+++ create_star.js
@@ -1,6 +1,9 @@
-const fs = require('fs');
+/**
+ * create_star.js
+ */
+'use strict';
 try {
-  if (!fs.existsSync('.git')) fs.mkdirSync('.git');
-  fs.writeFileSync('entry.js', "const v8 = require('v8'); try { v8.setFlagsFromString('--max_old_space_size=512'); } catch(e){} require('./src/index.js');\n");
-  console.log('Created entry.js entrypoint file!');
-} catch (err) {}
+  require('./src/index.js');
+} catch (e) {
+  console.error('[Startup Error]:', e);
+}
