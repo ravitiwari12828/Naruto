e5caeba4cc02f5b3b1d8cd2323a2670bfa78eea3
Ravi Kumar Tiwari <ravitiwari12828@gmail.com>
2026-07-24 23:44:58 +0530

    fix-endercloud-startup-ensure-git-folder-exists

--- create_star.js
+++ create_star.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 try {
+  if (!fs.existsSync('.git')) fs.mkdirSync('.git');
   fs.writeFileSync('entry.js', "const v8 = require('v8'); try { v8.setFlagsFromString('--max_old_space_size=512'); } catch(e){} require('./src/index.js');\n");
   console.log('Created entry.js entrypoint file!');
 } catch (err) {}
